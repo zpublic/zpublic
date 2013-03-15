14bf710cb92e8628b86da1a9c209b150a9b5a655
Sum <308974268>
2013-03-15 19:18:07 +0800

    加入代码生成器

---
+++ ScProduce/ScProduce/parser_def.h
@@ -0,0 +1,22 @@
+#ifndef SC_PARSER_DEF
+#define SC_PARSER_DEF
+
+#include <string>
+
+namespace zl
+{
+namespace sc
+{
+
+typedef enum _ErrParser
+{
+    ERR_NULL,
+    ERR_OPEN,
+    ERR_PARSER,
+    ERR_MISMATCH,
+}ErrParser;
+
+}
+}
+
+#endif
