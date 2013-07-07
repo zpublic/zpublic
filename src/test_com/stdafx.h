d910bea837c81140f9577d294f713c4c29b51909
zapline <278998871@qq.com>
2013-07-07 15:24:44 +0800

    + new project for learn COM

---
+++ src/test_com/stdafx.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+
+///> 标准容器
+#include <tuple>
+#include <array>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+using std::string;
+
+///> 输入输出相关
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using std::cout;
+using std::endl;
+
+///> zpublic！
+#include "zpublic.hpp"
---
+++ src/test_com/test_com.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+int wmain(int _Argc, wchar_t ** _Argv, wchar_t ** _Env)
+{
+    cout<<"hello2"<<endl;
+
+    getchar();
+    return 0;
+}
