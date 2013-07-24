cfb001e976a36eae43a1bdd9d4d9a678f0c7d32b
AngryPowman <138001655@qq.com>
2013-07-24 19:44:22 +0800

    * 增加消息处理映射表

--- src/irene/common/irene_common.h
+++ src/irene/common/irene_common.h
@@ -5,6 +5,20 @@
 	#define SERVER_DECL __declspec(dllexport)
 #endif
 
+#include <boost/asio.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/noncopyable.hpp>
+
+#include <string>
+#include <functional>
+#include <thread>
+#include <iostream>
+#include <stdint.h>
+#include <hash_map>
+
+#include <google/protobuf/message.h>
+
 //types
 #ifndef byte
     typedef unsigned char byte;
---
+++ src/irene/iserver/broiler_session.h
@@ -0,0 +1,14 @@
+#ifndef BROILER_SESSION_H_
+#define BROILER_SESSION_H_
+
+#include <irene_common.h>
+
+class BroilerSession
+{
+public:
+    void login_request_handler(const google::protobuf::Message& message)
+    {
+    }
+};
+
+#endif
