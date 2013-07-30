a76fba9732d71aa57b16aec6cb9fc4f3d5929b8a
AngryPowman <138001655@qq.com>
2013-07-30 18:00:30 +0800

    * 修改对象池支持带参数构造

--- src/irene/common/object_pool.hpp
+++ src/irene/common/object_pool.hpp
@@ -33,11 +33,17 @@ public:
 public:
     _Ty* acquire()
     {
-        if (_free_objects.empty())
-            allocate(_increase_size);
+        try_allocate();
+        _Ty* obj = get_free();
+        obj = new(obj)  _Ty();
+        return obj;
+    }
 
-        _Ty* obj = _free_objects.front();
-        _free_objects.pop();
+    template <typename ArgT0>
+    _Ty* acquire(const ArgT0& arg1)
+    {
+        try_allocate();
+        _Ty* obj = get_free();
 
         return obj;
     }
@@ -48,15 +54,35 @@ public:
     }
 
 private:
-    void allocate(uint32_t size)
+    _Ty* get_free()
     {
-        _Ty* obj = new _Ty[size];
+        _Ty* obj = _free_objects.front();
+        _free_objects.pop();
+
+        return obj;
+    }
+
+    void try_allocate()
+    {
+        if (_free_objects.empty())
+            allocate(_increase_size);
+    }
+
+    void allocate(size_t size)
+    {
+        _Ty* obj = malloc_obj(size);
+
         _all_objects.push_back(obj);
         for (size_t i = 0; i < size; ++i)
         {
             _free_objects.push(&obj[i]);
         }
-     }
+    }
+
+    _Ty* malloc_obj(size_t size)
+    {
+        return static_cast<_Ty*>(malloc(size));
+    }
 
 private:
     uint32_t _chunk_size;
