1f9e6efca39d83fbceafcfd1afff90f52c882f46
zapline <278998871@qq.com>
2013-09-01 13:34:07 +0800

    + study gdi for gui 3

---
+++ src/test_gui/gdi3.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "resource.h"
+
+class CGdi3 : public CWindowImpl<CGdi3>
+{
+public:
+    DECLARE_WND_CLASS(L"CGdi3")
+
+    BEGIN_MSG_MAP(CGdi3)
+        MSG_WM_CREATE(OnCreate)
+        MSG_WM_KEYDOWN(OnKeyDown)
+        MSG_WM_LBUTTONDOWN(OnLButtonDown)
+    END_MSG_MAP()
+
+private:
+    int OnCreate(LPCREATESTRUCT lpCreateStruct)
+    {
+        SetWindowLong(GWL_EXSTYLE, GetWindowLong(GWL_EXSTYLE) | WS_EX_LAYERED);
+        COLORREF clTransparent = RGB(255, 255, 255);
+        ::SetLayeredWindowAttributes(m_hWnd, clTransparent, 0, LWA_COLORKEY);
+        return 0;
+    }
+
+    void OnFinalMessage(HWND hWnd)
+    {
+        ::PostQuitMessage(0);
+    }
+
+    void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+    {
+        if (VK_ESCAPE == nChar)
+        {
+            DestroyWindow();
+        }
+        else
+        {
+            SetMsgHandled(FALSE);
+        }
+    }
+
+    void OnLButtonDown(UINT nFlags, CPoint point)
+    {
+        PostMessage(WM_SYSCOMMAND, SC_MOVE | HTCAPTION, 0);
+        SetMsgHandled(FALSE);
+    }
+};
+
+void gdi3()
+{
+    HBITMAP hBitmap = (HBITMAP)LoadImage(
+        g_module.m_hInst,
+        MAKEINTRESOURCE(IDB_BITMAP2),
+        IMAGE_BITMAP,
+        0,
+        0,
+        LR_CREATEDIBSECTION);
+    BITMAP bm;
+    GetObject(hBitmap, sizeof(bm), &bm);
+
+    CGdi3 dlg;
+    CWndClassInfo& wc = dlg.GetWndClassInfo();
+    wc.m_wc.hbrBackground = CreatePatternBrush(hBitmap);
+    CRect rect(0, 0, bm.bmWidth, bm.bmHeight);
+    dlg.Create(0, rect, L"CGdi3 wnd", WS_POPUP, WS_EX_TOPMOST);
+    dlg.CenterWindow();
+    dlg.ShowWindow(SW_SHOW);
+    dlg.UpdateWindow();
+
+    MSG msg;
+    msg.message = ~(UINT)WM_QUIT;
+    while(msg.message != WM_QUIT)
+    {
+        if(::GetMessage(&msg, NULL, 0, 0))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+        }
+    }
+}
--- src/test_gui/test_gdi.cpp
+++ src/test_gui/test_gdi.cpp
@@ -2,9 +2,11 @@
 #include "test_gdi.h"
 #include "gdi1.h"
 #include "gdi2.h"
+#include "gdi3.h"
 
 void test_gdi()
 {
     //gdi1();
-    gdi2();
+    //gdi2();
+    gdi3();
 }
