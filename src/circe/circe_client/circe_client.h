4f31492f0794a78ac0dd60f26c0b9193095b0334
zapline <278998871@qq.com>
2013-10-17 14:52:01 +0800

    + add circe_client 用wtl拖了两个对话框

---
+++ src/circe/circe_client/circe_client.h
@@ -0,0 +1,1 @@
+// circe_client.h
---
+++ src/circe/circe_client/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 circe_client.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_MAINDLG                     129
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_EDIT3                       1002
+#define IDC_RADIO1                      1003
+#define IDC_RADIO2                      1004
+#define IDC_BUTTON1                     1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
