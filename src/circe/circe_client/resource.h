c8f411070001f32042ff01064d9cf2c02487e20f
zapline <278998871@qq.com>
2013-11-12 17:48:59 +0800

    * circe 随便加了几个按钮

--- src/circe/circe_client/resource.h
+++ src/circe/circe_client/resource.h
@@ -19,6 +19,16 @@
 #define IDC_JUMP                        1005
 #define IDC_GIF1                        1006
 #define IDC_REG                         1007
+#define IDC_BUTTON2                     1008
+#define IDC_BUTTON3                     1009
+#define IDC_BUTTON4                     1010
+#define IDC_BUTTON5                     1011
+#define IDC_BUTTON6                     1012
+#define IDC_BUTTON7                     1013
+#define IDC_BUTTON8                     1014
+#define IDC_BUTTON9                     1015
+#define IDC_BUTTON10                    1016
+#define IDC_BUTTON11                    1017
 
 // Next default values for new objects
 // 
@@ -26,7 +36,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        207
 #define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
