a7ee6143a9f86a2222b576080588c6c0a1abb066
sum <xukala@qq.com>
2013-12-13 23:15:19 +0800

    [*] 修改点  coeus [*] 加入MAIN对话框

---
+++ src/coeus/client/game_main_dlg.cpp
@@ -0,0 +1,52 @@
+/* -------------------------------------------------------------------------
+//  File        :   game_main_dlg.cpp
+//  Author      :   Sum
+//  Datet       :   2013/12/13 21:58
+//  Brief       :   
+//
+// -----------------------------------------------------------------------*/
+//
+#include "stdafx.h"
+#include "game_main_dlg.h"
+#include "bkwinres.h"
+
+#define WINDOW_MAX_WIDTH            460
+#define WINDOW_MAX_HEIGHT           465
+
+#define WINDOW_MIN_WIDTH            460
+#define WINDOW_MIN_HEIGHT           55
+
+#define TIMER_BUTTON_MOVE           1
+
+#define NUM_BUFFER_MAX              260
+
+LRESULT GameMainDlg::OnInitDialog(HWND hWnd, LPARAM lParam)
+{
+    LoadIcon(NULL, MAKEINTRESOURCE(IDI_MIN_MAIN));
+
+    return TRUE;
+}
+
+void GameMainDlg::OnClose()
+{
+    EndDialog(IDOK);
+}
+
+void GameMainDlg::OnSysCommand(UINT nID, CPoint point)
+{
+    SetMsgHandled(FALSE);
+
+    if (nID == SC_CLOSE)
+    {
+        EndDialog(IDOK);
+    }
+}
+
+void GameMainDlg::OnTimer(UINT_PTR nIDEvent)
+{
+}
+
+void GameMainDlg::OnBtnClose()
+{
+
+}
---
+++ src/coeus/client/main.cpp
@@ -0,0 +1,45 @@
+// coeus.cpp : main source file for coeus.exe
+//
+
+#include "stdafx.h"
+
+#include <atlframe.h>
+#include <atlctrls.h>
+#include <atldlgs.h>
+#include "game_main_dlg.h"
+
+#include "resource.h"
+
+CAppModule _Module;
+
+int Run(LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
+{
+    GameMainDlg dlg;
+    return dlg.DoModal();
+}
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
+{
+
+    HRESULT hRes = ::CoInitialize(NULL);
+    ATLASSERT(SUCCEEDED(hRes));
+
+    ::DefWindowProc(NULL, 0, 0, 0L);
+
+    AtlInitCommonControls(ICC_BAR_CLASSES);
+
+    hRes = _Module.Init(NULL, hInstance);
+    ATLASSERT(SUCCEEDED(hRes));
+
+    ///> ≥ı ºªØBKWIN
+    BkString::Load(IDR_BK_STRING_DEF);
+    BkSkin::LoadSkins(IDR_BK_SKIN_DEF);
+    BkStyle::LoadStyles(IDR_BK_STYLE_DEF);
+
+    int nRet = Run(lpstrCmdLine, nCmdShow);
+
+    _Module.Term();
+    ::CoUninitialize();
+
+    return nRet;
+}
