ccbbe77dc7aa8a5594bb3534123143d127f7db7b
zapline <278998871@qq.com>
2013-12-13 15:58:03 +0800

    + 增加协议定义工具 CoreSword Editor

---
+++ src/coeus/tools/CoreSword_Editor/GeneratedCode/ErrorCode/cpp/ErrorCode.h
@@ -0,0 +1,11 @@
+// Generate by the sword of core(核心之刃). DO NOT EDIT!
+// Source : ErrorCode.h
+// Date : 2012/6/4 12:25:27
+// Comment : 在此处输入一些备注信息...
+
+namespace ErrorCode
+{
+        const unsigned int ERR_LOGIN_FAILED           = 1;   //登录失败
+        const unsigned int ERR_LOGIN_INVALID_USERNAME = 2;   //用户名无效
+        const unsigned int ERR_LOGIN_INVALID_PASSWORD = 3;   //密码无效
+}
