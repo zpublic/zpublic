eca30750ab61a7c2bcd268d632b15bcf7d2e6177
AngryPowman <138001655@qq.com>
2013-12-15 14:20:53 +0800

    tcp connection implementation

---
+++ src/coeus/venus_net/tcp_connection.cpp
@@ -0,0 +1,37 @@
+#include "tcp_connection.h"
+#include "Poco/Util/Application.h"
+#include "service_application.h"
+
+TcpConnection::TcpConnection(const Poco::Net::StreamSocket& socket)
+    : Poco::Net::TCPServerConnection(socket), _buffer(new byte[MAX_RECV_LEN])
+{
+    _socket = &this->socket();
+    _socket->setBlocking(false);
+}
+
+TcpConnection::~TcpConnection()
+{
+    SAFE_DELETE_ARR(_buffer);
+}
+
+void TcpConnection::run()
+{
+    try
+    {
+        for (;;)
+        {
+            int bytes_transferred = _socket->receiveBytes(_buffer, MAX_RECV_LEN, 0);
+            if (bytes_transferred == 0)
+            {
+                std::cout << "connection lost :(" << std::endl;
+                return;
+            }
+
+            std::cout << "received " << bytes_transferred << " bytes." << std::endl;
+        }
+    }
+    catch (Poco::Exception& e)
+    {
+        std::cout << "connection exception : " + e.message() << std::endl;
+    }
+}
