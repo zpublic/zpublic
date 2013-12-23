bc0a031d84472f290f99d6a1bd74a66cde4d1521
zapline <278998871@qq.com>
2013-12-23 20:38:17 +0800

    * coeus接入circe_net

---
+++ src/coeus/client/network_mgr.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "network_handler.h"
+
+class CNetworkMgr
+{
+public:
+    CNetworkMgr();
+
+    int Init();
+    int UnInit();
+
+    int Start(LPCWSTR lpAddr, unsigned int port);
+    int Stop();
+
+    int Send(const BYTE* pBuffer, int iLength);
+    int Send(const char* pBuffer, int iLength);
+
+private:
+    CNetworkHandler m_handler;
+    IClientNetwork* m_pClient;
+    HMODULE         m_hMod;
+    BOOL            m_bConnected;
+};
