493c87472bd2d89d55a7108fc2a8a145ce988b39
沙包妖梦 <gongteng524702837@gmail.com>
2014-01-27 14:47:08 +0800

    分离用户系统php

--- user_service/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('APP_STATUS','gongt');
-define('APP_DEBUG', true);
-
-define('BUILD_DIR_SECURE', false);
-define('APP_PATH', './Application/');
-define('RUNTIME_PATH', './.runtime/');
-require 'Think/ThinkPHP/ThinkPHP.php';
--- user_service/server.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('APP_STATUS', '');
-define('APP_DEBUG', false);
-
-define('BUILD_DIR_SECURE', false);
-define('APP_PATH', './Application/');
-define('RUNTIME_PATH', '/tmp/runtime/');
-require 'Think/ThinkPHP/ThinkPHP.php';
