479ed9c24a3b9680fe16f9b51a3a289e594237b7
zapline <278998871@qq.com>
2014-06-16 15:14:19 +0800

    * 整理zl::DesignPattern代码

--- pellets/design_pattern/factory.h
+++ pellets/design_pattern/factory.h
@@ -1,5 +1,26 @@
+/*************************************************************************
+ *                                                                       *
+ *  I|*j^3Cl|a   "+!*%                  qt          Nd   gW              *
+ *  l]{y+l?MM*  !#Wla\NNP               NW          MM   I|              *
+ *        PW    ?E|    tWg              Wg  sC!     AW           ~@v~    *
+ *       NC     ?M!    yN|  WW     MK   MW@K1Y%M@   RM   #Q    QP@tim    *
+ *     CM|      |WQCljAE|   MD     Mg   RN     cM~  NM   WQ   MQ         *
+ *    #M        aQ?         MW     M3   Mg      Q(  HQ   YR  IM|         *
+ *   Dq         {Ql         MH    iMX   Mg     MM   QP   QM   Eg         *
+ * !EWNaPRag2$  +M"          $WNaHaN%   MQE$%EXW    QQ   CM    %M%a$D    *
+ *                                                                       *
+ *                               ZPublic                                 *
+ *                  Developer: zapline(278998871@qq.com)                 *
+ *               Website: https://github.com/zpublic/zpublic             *
+ *                                                                       *
+ ************************************************************************/
 #pragma once
 #include <map>
+
+namespace zl
+{
+namespace DesignPattern
+{
 
 template <typename T, typename T2>
 T* CreateObject()
@@ -44,3 +65,6 @@ public:
 private:
     std::map<TKey, fnCreateObject> m_factory;
 };
+
+}
+}
--- src/test_pellets/TestDesignPattern.h
+++ src/test_pellets/TestDesignPattern.h
@@ -12,51 +12,51 @@
 extern int gDesignPatternTestNum;
 
 ///> Observer 观察者模式
-class Observer1 : public Observer
+class Observer1 : public zl::DesignPattern::Observer
 {
 public:
-    virtual void update( Observable* p, void* lpData )
+    virtual void update( zl::DesignPattern::Observable* p, void* lpData )
     {
         gDesignPatternTestNum += (int)lpData;
     }
 };
-class Observer2 : public Observer
+class Observer2 : public zl::DesignPattern::Observer
 {
 public:
-    virtual void update( Observable* p, void* lpData )
+    virtual void update( zl::DesignPattern::Observable* p, void* lpData )
     {
         gDesignPatternTestNum += (int)lpData + 1;
     }
 };
-class ObservableX : public Observable
+class ObservableX : public zl::DesignPattern::Observable
 {
 };
 
 ///> Strategy 策略模式
-class Strategy1 : public Strategy
+class Strategy1 : public zl::DesignPattern::Strategy
 {
 public:
-    virtual void* Operate(StrategyContext* p, void* lpData)
+    virtual void* Operate(zl::DesignPattern::StrategyContext* p, void* lpData)
     {
         int n = (int)lpData + 1;
         return (void*)n;
     }
 };
-class Strategy2 : public Strategy
+class Strategy2 : public zl::DesignPattern::Strategy
 {
 public:
-    virtual void* Operate(StrategyContext* p, void* lpData)
+    virtual void* Operate(zl::DesignPattern::StrategyContext* p, void* lpData)
     {
         int n = (int)lpData + 2;
         return (void*)n;
     }
 };
-class StrategyContextX : public StrategyContext
+class StrategyContextX : public zl::DesignPattern::StrategyContext
 {
 };
 
 ///> Decorator 装饰模式
-class ConcreteComponentX : public ConcreteComponent
+class ConcreteComponentX : public zl::DesignPattern::ConcreteComponent
 {
 public:
     virtual void Operation(void* lpData)
@@ -64,7 +64,7 @@ public:
         *(int*)lpData = 1;
     }
 };
-class Decorator1 : public Decorator
+class Decorator1 : public zl::DesignPattern::Decorator
 {
 public:
     virtual void Operation(void* lpData)
@@ -73,7 +73,7 @@ public:
         *(int*)lpData += 1;
     }
 };
-class Decorator2 : public Decorator
+class Decorator2 : public zl::DesignPattern::Decorator
 {
 public:
     virtual void Operation(void* lpData)
@@ -108,10 +108,10 @@ public:
         return 1;
     }
 };
-typedef Singleton<SingletonXX> SingletonX;
+typedef zl::DesignPattern::Singleton<SingletonXX> SingletonX;
 
 ///> Command 命令模式
-class CommandAdd1 : public CommandBase
+class CommandAdd1 : public zl::DesignPattern::CommandBase
 {
 public:
     virtual void* Execute(void* lpData)
@@ -120,7 +120,7 @@ public:
         return 0;
     }
 };
-class CommandAdd10 : public CommandBase
+class CommandAdd10 : public zl::DesignPattern::CommandBase
 {
 public:
     virtual void* Execute(void* lpData)
@@ -299,8 +299,8 @@ public:
         Observer1 o1;
         Observer2 o2;
         ObservableX x;
-        x.addObserver(static_cast<Observer*>(&o1));
-        x.addObserver(static_cast<Observer*>(&o2));
+        x.addObserver(static_cast<zl::DesignPattern::Observer*>(&o1));
+        x.addObserver(static_cast<zl::DesignPattern::Observer*>(&o2));
         x.setChanged();
         x.notifyObservers((void*)1);
         TEST_ASSERT(gDesignPatternTestNum = 3);
@@ -309,7 +309,7 @@ public:
         x.setChanged();
         x.notifyObservers((void*)2);
         TEST_ASSERT(gDesignPatternTestNum = 8);
-        x.deleteObserver(static_cast<Observer*>(&o2));
+        x.deleteObserver(static_cast<zl::DesignPattern::Observer*>(&o2));
         x.setChanged();
         x.notifyObservers((void*)2);
         TEST_ASSERT(gDesignPatternTestNum = 10);
@@ -322,10 +322,10 @@ public:
         StrategyContextX x;
         void* ret = x.Operate(0);
         TEST_ASSERT((int)ret == 0);
-        x.SetStrategy(static_cast<Strategy*>(&s1));
+        x.SetStrategy(static_cast<zl::DesignPattern::Strategy*>(&s1));
         ret = x.Operate(0);
         TEST_ASSERT((int)ret == 1);
-        x.SetStrategy(static_cast<Strategy*>(&s2));
+        x.SetStrategy(static_cast<zl::DesignPattern::Strategy*>(&s2));
         ret = x.Operate(0);
         TEST_ASSERT((int)ret == 2);
     }
@@ -338,23 +338,23 @@ public:
         int n = 0;
         x.Operation((void*)&n);
         TEST_ASSERT(n == 1);
-        d1.Decorate(static_cast<DecoratorComponent*>(&x));
+        d1.Decorate(static_cast<zl::DesignPattern::DecoratorComponent*>(&x));
         d1.Operation((void*)&n);
         TEST_ASSERT(n == 2);
-        d2.Decorate(static_cast<DecoratorComponent*>(&x));
+        d2.Decorate(static_cast<zl::DesignPattern::DecoratorComponent*>(&x));
         d2.Operation((void*)&n);
         TEST_ASSERT(n == 3);
-        d2.Decorate(static_cast<DecoratorComponent*>(&d1));
+        d2.Decorate(static_cast<zl::DesignPattern::DecoratorComponent*>(&d1));
         d2.Operation((void*)&n);
         TEST_ASSERT(n == 4);
     }
 
     void testSimpleFactory()
     {
-        SimpleFactory<Product> f;
-        SimpleFactory<Product>::fnCreateObject fn = CreateObject<Product, Product1>;
+        zl::DesignPattern::SimpleFactory<Product> f;
+        zl::DesignPattern::SimpleFactory<Product>::fnCreateObject fn = zl::DesignPattern::CreateObject<Product, Product1>;
         f.AddCreator(1, fn);
-        f.AddCreator(2, CreateObject<Product, Product2>);
+        f.AddCreator(2, zl::DesignPattern::CreateObject<Product, Product2>);
         Product* p1 = f.CreateObject(1);
         TEST_ASSERT(p1->GetX() == 100);
         delete p1;
@@ -364,10 +364,10 @@ public:
         Product* p3 = f.CreateObject(3);
         TEST_ASSERT(p3 == NULL);
 
-        SimpleFactory<Product, std::string> f2;
-        SimpleFactory<Product, std::string>::fnCreateObject fn2 = CreateObject<Product, Product1>;
+        zl::DesignPattern::SimpleFactory<Product, std::string> f2;
+        zl::DesignPattern::SimpleFactory<Product, std::string>::fnCreateObject fn2 = zl::DesignPattern::CreateObject<Product, Product1>;
         f2.AddCreator("a", fn);
-        f2.AddCreator("b", CreateObject<Product, Product2>);
+        f2.AddCreator("b", zl::DesignPattern::CreateObject<Product, Product2>);
         Product* p11 = f2.CreateObject("a");
         TEST_ASSERT(p11->GetX() == 100);
         delete p11;
@@ -386,7 +386,7 @@ public:
 
     void testCommand()
     {
-        CommandInvoker invoker;
+        zl::DesignPattern::CommandInvoker invoker;
         CommandAdd1 c1;
         CommandAdd10 c10;
         gDesignPatternTestNum = 0;
@@ -398,11 +398,11 @@ public:
         TEST_ASSERT(gDesignPatternTestNum == 11);
         invoker.Invoke();
         TEST_ASSERT(gDesignPatternTestNum == 21);
-        EmptyCommand cc1;
+        zl::DesignPattern::EmptyCommand cc1;
         invoker.SetCommand(&cc1);
         invoker.Invoke();
         TEST_ASSERT(gDesignPatternTestNum == 21);
-        MacroCommand cc2;
+        zl::DesignPattern::MacroCommand cc2;
         cc2.AddCommand(&c1);
         cc2.AddCommand(&c1);
         cc2.AddCommand(&c10);
