95b1606c989fcd27cde7b443b910e1ae8844b5d0
sum <308974268@qq.com>
2014-07-03 16:26:54 +0800

    * 完成注册表封装 完成注册表相关单元测试

--- pellets/z_win_utils/register.hpp
+++ pellets/z_win_utils/register.hpp
@@ -23,11 +23,294 @@ namespace WinUtils
     class Registe
     {
     public:
-        Registe();
-        ~Registe();
+        Registe()
+            : hKey_(NULL)
+            , bIsAttach_(FALSE)
+        {
+        }
+
+        ~Registe()
+        {
+            Close();
+        }
+
+    public:
+        void Attach(HKEY hKey)
+        {
+            Close();
+            hKey_ = hKey;
+            bIsAttach_ = TRUE;
+        }
+
+        HKEY Detach(void)
+        {
+            bIsAttach_ = FALSE;
+            HKEY hKey = hKey_;
+            hKey_ = NULL;
+            return hKey;
+        }
+
+        BOOL Open(HKEY hRootKey, LPCTSTR szSubKey, BOOL bReadOnly = TRUE, REGSAM samDesired = KEY_READ | KEY_WRITE)
+        {
+            Close();
+            LONG lRetCode = 0;
+            if (bReadOnly)
+            {
+                lRetCode = ::RegOpenKeyEx(hRootKey,
+                    szSubKey,
+                    0,
+                    samDesired,
+                    &hKey_);
+            }
+            else
+            {
+                lRetCode = ::RegCreateKeyEx(hRootKey,
+                    szSubKey,
+                    0L,
+                    NULL,
+                    REG_OPTION_NON_VOLATILE,
+                    KEY_ALL_ACCESS, NULL, 
+                    &hKey_, NULL);
+            }
+            if (lRetCode != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
+
+        BOOL CreateVolatileReg(HKEY hRootKey, LPCTSTR szSubKey)
+        {
+            Close();
+            if (::RegCreateKeyEx(hRootKey,
+                szSubKey,
+                0L,
+                NULL,
+                REG_OPTION_VOLATILE,
+                KEY_ALL_ACCESS,
+                NULL,
+                &hKey_,
+                NULL) != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
+
+        BOOL DeleteKey(LPCTSTR szName)
+        {
+            if (ERROR_SUCCESS == ::RegDeleteTree(hKey_, szName))
+            {
+                return TRUE;
+            }
+            return FALSE;
+        }
+
+        BOOL DeleteValue(LPCTSTR szName)
+        {
+            if (ERROR_SUCCESS == ::RegDeleteValue(hKey_, szName))
+            {
+                return TRUE;
+            }
+            return FALSE;
+        }
+
+        void Close(void)
+        {
+            if (!bIsAttach_ && hKey_)
+            {
+                ::RegCloseKey(hKey_);
+                hKey_ = NULL;
+            }
+            else
+            {
+                hKey_ = NULL;
+            }
+        }
+
+        BOOL Read(LPCTSTR pszValueName, DWORD& dwValue)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL)
+            {
+                return FALSE;
+            }
+
+            DWORD dwDest = 0;
+            DWORD dwType = 0;
+            DWORD dwSize = sizeof (DWORD);
+            if (::RegQueryValueEx(hKey_,
+                pszValueName,
+                NULL,
+                &dwType,
+                (BYTE *)&dwDest,
+                &dwSize) == ERROR_SUCCESS)
+            {
+                if (dwType == REG_DWORD)
+                {
+                    dwValue = dwDest;
+                    return TRUE;
+                }
+            }
+            return FALSE;
+        }
+
+        BOOL Read(LPCTSTR pszValueName, CString& strValue)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL)
+            {
+                return FALSE;
+            }
+
+            BOOL bReturn = FALSE;
+            DWORD dwType;
+            DWORD dwSize = 0;
+            do
+            {
+                if (::RegQueryValueEx(hKey_,
+                    pszValueName,
+                    NULL,
+                    &dwType,
+                    NULL,
+                    &dwSize) != ERROR_SUCCESS)
+                {
+                    break;
+                }
+                if (dwType != REG_SZ
+                    && dwType != REG_EXPAND_SZ)
+                {
+                    break;
+                }
+                strValue.Empty();
+                LPTSTR pBuffer = strValue.GetBuffer(dwSize / 2 + 1);
+                if (!pBuffer)
+                {
+                    break;
+                }
+                if (::RegQueryValueEx(hKey_,
+                    pszValueName,
+                    NULL,
+                    &dwType,
+                    (BYTE*)pBuffer,
+                    &dwSize) == ERROR_SUCCESS)
+                {
+                    pBuffer[dwSize / 2] = 0;
+                    bReturn = TRUE;
+                }
+                strValue.ReleaseBuffer();
+            } while (FALSE);
+            return bReturn;
+        }
+
+        BOOL Read(LPCTSTR pszValueName, BYTE* pBuffer, DWORD& dwSize)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL)
+            {
+                return FALSE;
+            }
+
+            BOOL bReturn = FALSE;
+            LONG lRetCode = 0;
+            DWORD dwType = 0;
+            if (::RegQueryValueEx(hKey_,
+                pszValueName,
+                NULL,
+                &dwType,
+                pBuffer,
+                &dwSize) != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
+
+        BOOL Write(LPCTSTR pszValueName, DWORD dwValue)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL)
+            {
+                return FALSE;
+            }
+
+            BOOL bReturn = FALSE;
+            if (::RegSetValueEx(hKey_,
+                pszValueName,
+                NULL,
+                REG_DWORD,
+                (CONST BYTE*)&dwValue,
+                sizeof(DWORD)) != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
+
+        BOOL Write(LPCTSTR pszValueName, LPCTSTR pszValue)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL
+                || pszValue == NULL)
+            {
+                return FALSE;
+            }
+            if (::RegSetValueEx(hKey_,
+                pszValueName,
+                NULL,
+                REG_SZ,
+                (CONST BYTE*) pszValue,
+                ((int)_tcslen(pszValue) + 1) * sizeof(TCHAR)) != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
+
+        BOOL WriteExpandString(LPCTSTR pszValueName, LPCTSTR pszValue)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL
+                || pszValue == NULL)
+            {
+                return FALSE;
+            }
+            if (::RegSetValueEx(hKey_,
+                pszValueName,
+                NULL,
+                REG_EXPAND_SZ,
+                (CONST BYTE*) pszValue,
+                ((int)_tcslen(pszValue) + 1) * sizeof(TCHAR)) != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
+
+        BOOL Write(LPCTSTR pszValueName, BYTE* pBuffer, DWORD dwSize)
+        {
+            if (pszValueName == NULL
+                || hKey_ == NULL
+                || pBuffer == NULL)
+            {
+                return FALSE;
+            }
+            if (::RegSetValueEx(hKey_,
+                pszValueName,
+                NULL,
+                REG_BINARY,
+                pBuffer,
+                dwSize) != ERROR_SUCCESS)
+            {
+                return FALSE;
+            }
+            return TRUE;
+        }
 
     private:
-
+        HKEY hKey_;
+        BOOL bIsAttach_;
     };
+
 }
 }
