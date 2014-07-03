4f6bd6f8815d9073e50992264d11fe6eed39b8cc
sum <308974268@qq.com>
2014-07-03 12:20:45 +0800

    *先保存一下代码 并生成base分支

---
+++ pellets/z_win_utils/register.hpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+ *                                                                       *
+ *  I|*j^3Cl|a   "+!*%                  qt          Nd   gW              *
+ *  l]{y+l?MM*  !#Wla\NNP               NW          MM   I|              *
+ *        PW    ?E|    tWg              Wg  sC!     AW           ~@v~    *
+ *       NC     ?M!    yN|  WW     MK   MW@K1Y%M@   RM   #Q    QP@tim    *
+ *     CM|      |WQCljAE|   MD     Mg   RN     cM~  NM   WQ   MQ         *
+ *    #M        aQ?         MW     M3   Mg      Q(  HQ   YR  IM|         *
+ *   Dq         {Ql         MH    iMX   Mg     MM   QP   QM   Eg         *
+ * !EWNaPRag2$  +M"          $WNaHaN%   MQE$%EXW    QQ   CM    %M%a$D    *
+ *                                                                       *
+ *               Website: https://github.com/zpublic/zpublic             *
+ *                                                                       *
+ ************************************************************************/
+#pragma once
+#include "win_utils_header.h"
+
+namespace zl
+{
+namespace WinUtils
+{
+
+    class Registe
+    {
+    public:
+        Registe();
+        ~Registe();
+
+    private:
+
+    };
+}
+}
