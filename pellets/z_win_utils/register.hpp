78dfdbc73e37990bb4cedf16a476c91b53838794
sum <308974268@qq.com>
2014-07-11 17:26:03 +0800

    * 替换 RegDeleteTree 为 SHDeleteKey

--- pellets/z_win_utils/register.hpp
+++ pellets/z_win_utils/register.hpp
@@ -21,6 +21,7 @@
 
 #pragma once
 #include "win_utils_header.h"
+#include <shlwapi.h>
 
 namespace zl
 {
@@ -97,7 +98,7 @@ namespace WinUtils
             {
                 return FALSE;
             }
-            if (ERROR_SUCCESS == ::RegDeleteTree(hKey_, pszName))
+            if (ERROR_SUCCESS == ::SHDeleteKey(hKey_, pszName))
             {
                 return TRUE;
             }
