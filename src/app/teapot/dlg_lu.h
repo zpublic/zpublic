3b30d2a6126aa2f700752f72ffb2f3331dbb02d5
zapline <278998871@qq.com>
2014-07-20 22:26:43 +0800

    * teapot从json读取数据展示到对话框

--- src/app/teapot/dlg_lu.h
+++ src/app/teapot/dlg_lu.h
@@ -30,10 +30,26 @@ private:
             Json::Value v;
             Json::Reader reader;
             bool bRet = reader.parse((const char *)m.GetData(), (const char *)m.GetData() + m.GetLength(), v);
-            bRet = true;
+            if (bRet)
+            {
+                UpdateUi(v);
+            }
         }
 
         return 1;
+    }
+
+    void UpdateUi(Json::Value& v)
+    {
+        SetDlgItemText(IDC_STATIC_TITLE, CA2W(v["title"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T1, CA2W(v["comment"]["1"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T2, CA2W(v["comment"]["2"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T3, CA2W(v["comment"]["3"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T4, CA2W(v["comment"]["4"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T5, CA2W(v["comment"]["5"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T6, CA2W(v["comment"]["6"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T7, CA2W(v["comment"]["7"].asString().c_str(), CP_ACP));
+        SetDlgItemText(IDC_STATIC_T8, CA2W(v["comment"]["8"].asString().c_str(), CP_ACP));
     }
 
     LRESULT OnOk(WORD, UINT, HWND, BOOL&)
