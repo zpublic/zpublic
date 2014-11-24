e2f032f4a1e4f99994560f7eb18a7dfbcabd9bea
siren186 <siren186@163.com>
2014-11-24 10:32:05 +0800

    * 将与平台相关的代码实现部分,移到.hpp文件中.

--- pellets/z_platform/z_platform_mutex.h
+++ pellets/z_platform/z_platform_mutex.h
@@ -2,9 +2,9 @@
 #include "z_platform_def.h"
 
 #ifdef Z_PLATFORM_WIN
-#include "z_platform_win_mutex.h"
+#include "z_platform_win_mutex.hpp"
 #else
-#include "z_platform_linux_mutex.h"
+#include "z_platform_linux_mutex.hpp"
 #endif
 
 class zl_mutex
