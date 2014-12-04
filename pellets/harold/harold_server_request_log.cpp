fac6c631a3ea674a7ad46cdb61bc33ff10c61f78
siren186 <siren186@163.com>
2014-12-04 19:21:05 +0800

    * 增删一些平台相关的代码,保证vs2013和安卓均能编译通过.

--- pellets/harold/harold_server_request_log.cpp
+++ pellets/harold/harold_server_request_log.cpp
@@ -1,16 +1,16 @@
-#include "stdafx.h"
+#include "harold_header.h"
 #include "harold_server_request_log.h"
 
 int HaroldServerRequestLog::OnRequest(struct mg_connection *conn)
 {
     assert(conn);
-    std::cout << "OnRequest " << conn->request_method << std::endl;
-    std::cout << "OnRequest " << conn->uri << std::endl;
+    HAROLD_PRINT("OnRequest:%s", conn->request_method);
+    HAROLD_PRINT("OnRequest:%s", conn->uri);
     if (conn->query_string)
     {
-        std::cout << "OnRequest " << conn->query_string << std::endl;
+        HAROLD_PRINT("OnRequest:%s", conn->query_string);
     }
-    std::cout << "OnRequest " << conn->remote_ip << std::endl;
-    std::cout << "OnRequest " << conn->local_ip << std::endl;
+    HAROLD_PRINT("OnRequest:%s", conn->remote_ip);
+    HAROLD_PRINT("OnRequest:%s", conn->local_ip);
     return 0;
 }
