07454258c122c3e478bbb4d66ee59868a98e4742
zapline <278998871@qq.com>
2014-12-30 17:39:35 +0800

    * kxlol 定时执行者调用脚本

--- src/app/kxlol/TimerBaiscTask.cpp
+++ src/app/kxlol/TimerBaiscTask.cpp
@@ -13,6 +13,19 @@ TimerBaiscTask::~TimerBaiscTask()
 
 void TimerBaiscTask::BasicMinuteTask()
 {
-    GAME.Player().BasicInfo().AddShenli(3);
-    // todo:更新界面
+    CString sPath = ZLPath::GetModuleFolder(NULL) + "data\\timer_basic_minute_task.lua";
+    if (::PathFileExists(sPath))
+    {
+        z_lua_state lua;
+        if (lua.create() == 0)
+        {
+            GAME.Script().RegAll(lua);
+            if (0 == lua.open_all_libs()
+                && 0 == lua.dofile(ZLW2A(sPath)))
+            {
+                int n = lua.call<int>("dowork");
+                ASSERT(n == 0);
+            }
+        }
+    }
 }
