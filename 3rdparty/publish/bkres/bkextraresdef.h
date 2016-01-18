0e4896b16e774d2f87e1fa80f1b9c5650b85c57e
zapline <278998871@qq.com>
2016-01-18 12:15:50 +0800

    - 去掉老版bw

--- 3rdparty/publish/bkres/bkextraresdef.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-interface IBkExtraResource
-{
-    virtual BOOL LoadModuleResource(LPCWSTR lpszResType, UINT uResID, IStream** ppiStmResource) = NULL;
-    virtual BOOL LoadGlobalResource(LPCWSTR lpszResType, UINT uResID, IStream** ppiStmResource) = NULL;
-	virtual BOOL LoadThemeResource(LPCWSTR lpszResType, UINT uResID, IStream** ppiStmResource) = NULL;
-};
